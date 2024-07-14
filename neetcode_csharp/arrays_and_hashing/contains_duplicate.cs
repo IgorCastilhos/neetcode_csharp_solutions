1d25372f16a637476541349642656e678f4e43c6
igorc <igorcastilhos2010@hotmail.com>
2024-07-14 03:03:02 -0300

    Add: array & hashing/contains duplicate

---
+++ neetcode_csharp/arrays_and_hashing/contains_duplicate.cs
@@ -0,0 +1,23 @@
+﻿namespace neetcode_csharp.arrays_and_hashing;
+
+public class ContainsDuplicate
+{
+    public bool CheckForDuplicate(int[] nums)
+    {
+        HashSet<int> seen = new HashSet<int>();
+        foreach (int num in nums)
+        {
+            if (!seen.Add(num))
+            {
+                return true;
+            }
+
+            seen.Add(num);
+        }
+
+        return false;
+    }
+}
+
+// Time complexity: O(n)
+// Space complexity: O(n)
